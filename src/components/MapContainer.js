3542a2911b6b237ac473126b43828ef647976fdc
zhannett <janetkulyk@yahoo.com>
2019-05-04 15:54:34 +0300

    Fixing Google maps

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 import MapWithAMarker from "./MapWithAMarker";
-// import keys from "./config/keys";
+import keys from "./config/keys";
 import media from "../mediaTemplate";
 
 const Map = styled.div`
@@ -21,18 +21,26 @@ const Map = styled.div`
 
 class MapContainer extends Component {
   render() {
+    const google_maps_api_key;
+    if (env === "development") {
+      google_maps_api_key = keys.google_maps_api_key;
+    }
+    if (env === "production") {
+      google_maps_api_key = process.env.GOOGLE_MAPS_API_KEY;
+    }
     return (
       <Map>
-        {/*
+        <h1> {process.env.NODE_ENV}</h1>
+        
         <MapWithAMarker
           googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${
-            keys.google_maps_api_key
+            google_maps_api_key
           }&v=3.exp&libraries=geometry,drawing,places`}
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `255px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
         />
-        */}
+        
       </Map>
     );
   }
