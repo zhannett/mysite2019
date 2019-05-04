04249caaf94a87f09ddf5b1964b915ddaf273fc3
zhannett <janetkulyk@yahoo.com>
2019-05-04 16:30:04 +0300

    Fixing Google maps

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 import MapWithAMarker from "./MapWithAMarker";
-// import keys from "./config/keys";
+import keys from "./config/keys";
 import media from "../mediaTemplate";
 
 const Map = styled.div`
@@ -18,14 +18,9 @@ const Map = styled.div`
     height: 260px;
   `};
 `;
-/*
-if (process.env.NODE_ENV === "development") {
-  const google_maps_api_key = keys.google_maps_api_key;
-} else 
-*/
-if (process.env.NODE_ENV === "production") {
-  const google_maps_api_key = GOOGLE_MAPS_API_KEY;
-}
+
+var google_maps_api_key =
+  process.env.GOOGLE_MAPS_API_KEY || keys.google_maps_api_key;
 
 class MapContainer extends Component {
   render() {
@@ -33,9 +28,7 @@ class MapContainer extends Component {
       <Map>
         <h1> {process.env.NODE_ENV}</h1>
         <MapWithAMarker
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${
-            keys.google_maps_api_key
-          }&v=3.exp&libraries=geometry,drawing,places`}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${google_maps_api_key}&v=3.exp&libraries=geometry,drawing,places`}
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `255px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
