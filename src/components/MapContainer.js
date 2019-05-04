36dc026a16f1f31054650bdfa1f183b9276e93d3
zhannett <janetkulyk@yahoo.com>
2019-05-04 16:39:13 +0300

    Finilize Google Maps?

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 import MapWithAMarker from "./MapWithAMarker";
-// import keys from "./config/keys";
 import media from "../mediaTemplate";
+if (process.env.NODE_ENV) {
+  import keys from "./config/keys";
+}
 
 const Map = styled.div`
   width: 44%;
@@ -19,14 +21,13 @@ const Map = styled.div`
   `};
 `;
 
-var google_maps_api_key = process.env.GOOGLE_MAPS_API_KEY;
-//  process.env.GOOGLE_MAPS_API_KEY || keys.google_maps_api_key;
+var google_maps_api_key =
+  process.env.GOOGLE_MAPS_API_KEY || keys.google_maps_api_key;
 
 class MapContainer extends Component {
   render() {
     return (
       <Map>
-        <h1> {process.env.NODE_ENV}</h1>
         <MapWithAMarker
           googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${google_maps_api_key}&v=3.exp&libraries=geometry,drawing,places`}
           loadingElement={<div style={{ height: `100%` }} />}
