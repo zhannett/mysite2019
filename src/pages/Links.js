8529ca7f16854791c7d22ed04a6cae2b068ef571
zhannett <janetkulyk@yahoo.com>
2019-05-05 17:27:08 +0300

    Putting links into json file

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import styled from "@emotion/styled";
+import styled from "styled-components";
 import { Link } from "@reach/router";
+
+import linksData from "../../src/linksData";
 import {
   Accordion,
   AccordionItem,
@@ -48,6 +50,24 @@ class Links extends Component {
     this.setState({ newwin: !this.state.newwin });
   }
 
+  renderSection(section, links) {
+    return (
+      <AccordionItem>
+        <AccordionItemHeading>
+          <AccordionItemButton>
+            <span />
+            {section}
+          </AccordionItemButton>
+        </AccordionItemHeading>
+        <AccordionItemPanel>
+          <ul>
+            {links.map(item => this.renderExternalLink(item.url, item.title))}
+          </ul>
+        </AccordionItemPanel>
+      </AccordionItem>
+    );
+  }
+
   renderExternalLink(href, title) {
     return (
       <li>
@@ -88,311 +108,9 @@ class Links extends Component {
             <Lists>
               <div className="links-container">
                 <Accordion allowZeroExpanded="true">
-                  <AccordionItem>
-                    <AccordionItemHeading>
-                      <AccordionItemButton>
-                        <span />
-                        General Links
-                      </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                      <ul>
-                        {this.renderExternalLink(
-                          "http://w3.org",
-                          "World Wide Web Consortium"
-                        )}
-                        {this.renderExternalLink(
-                          "http://validator.w3.org",
-                          "W3C HTML Validation Service"
-                        )}
-                        {this.renderExternalLink(
-                          "http://caniuse.com/",
-                          "Can I Use?"
-                        )}
-                        {this.renderExternalLink(
-                          "http://frontendmasters.com/",
-                          "Frontend Masters"
-                        )}
-                        {this.renderExternalLink(
-                          "http://egghead.io/",
-                          "Egg Head"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.sitepoint.com",
-                          "Site Point"
-                        )}
-                        {this.renderExternalLink(
-                          "http://developer.yahoo.com",
-                          "Yahoo Develolper Networkt"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.quirksmode.org",
-                          "www.quirksmode.org"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.w3schools.com/browsers/browsers_stats.asp",
-                          "Browser Statistics"
-                        )}
-                        {this.renderExternalLink(
-                          "https://www.creativebloq.com/net-magazine",
-                          "Browser Statistics Net Magazine"
-                        )}
-                      </ul>
-                    </AccordionItemPanel>
-                  </AccordionItem>
-
-                  <AccordionItem>
-                    <AccordionItemHeading>
-                      <AccordionItemButton>
-                        <span /> JavaScript
-                      </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                      <ul>
-                        {this.renderExternalLink(
-                          "http://javascript.crockford.com/",
-                          "Douglas Crockford"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.davidflanagan.com",
-                          "David Flanagan"
-                        )}
-                        {this.renderExternalLink(
-                          "http://howjavascriptworks.com/",
-                          "How JavaScript Works"
-                        )}
-                        {this.renderExternalLink(
-                          "https://medium.com/@jsmuster/awesome-es6-features-by-example-58974b356955",
-                          "Awesome ES6 Features By Example"
-                        )}
-                      </ul>
-                    </AccordionItemPanel>
-                  </AccordionItem>
-
-                  <AccordionItem>
-                    <AccordionItemHeading>
-                      <AccordionItemButton>
-                        <span /> React.js / Redux
-                      </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                      <ul>
-                        {this.renderExternalLink(
-                          "https://kentcdodds.com/blog",
-                          "Kent C. Dodds blog"
-                        )}
-                        {this.renderExternalLink(
-                          "https://css-tricks.com/using-react-portals-to-render-children-outside-the-dom-hierarchy/",
-                          "Using React Portals to Render Children Outside the DOM Hierarchy"
-                        )}
-                        {this.renderExternalLink(
-                          "https://medium.com/@jsmuster/understanding-reducers-d0f934aceccd",
-                          "Understanding Reducers from scratch"
-                        )}
-                        {this.renderExternalLink(
-                          "https://medium.com/@jsmuster/using-redux-standalone-497aa85981c1",
-                          "Using Redux standalone"
-                        )}
-                      </ul>
-                    </AccordionItemPanel>
-                  </AccordionItem>
-
-                  <AccordionItem>
-                    <AccordionItemHeading>
-                      <AccordionItemButton>
-                        <span /> AWS
-                      </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                      <ul>
-                        {this.renderExternalLink(
-                          "https://gist.github.com/stevekinney/6ab02582829f039b6a14c973923909f8",
-                          "AWS for Frontend Engineers"
-                        )}
-                        {this.renderExternalLink(
-                          "https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/GettingStarted.html",
-                          "Getting Started with CloudFront"
-                        )}
-                      </ul>
-                    </AccordionItemPanel>
-                  </AccordionItem>
-
-                  <AccordionItem>
-                    <AccordionItemHeading>
-                      <AccordionItemButton>
-                        <span /> HTML5
-                      </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                      <ul>
-                        {this.renderExternalLink(
-                          "http://html5boilerplate.com/",
-                          "HTML5 Boilerplate"
-                        )}
-                        {this.renderExternalLink(
-                          "http://html5doctor.com/avoiding-common-html5-mistakes/",
-                          "Avoiding Common HTML5 Mistakes"
-                        )}
-                      </ul>
-                    </AccordionItemPanel>
-                  </AccordionItem>
-                  <AccordionItem>
-                    <AccordionItemHeading>
-                      <AccordionItemButton>
-                        <span /> CSS3
-                      </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                      <ul>
-                        {this.renderExternalLink(
-                          "http://csszengarden.com",
-                          "CSS Zen Garden"
-                        )}
-                        {this.renderExternalLink(
-                          "https://medium.freecodecamp.org/web-fonts-in-2018-f191a48367e8",
-                          "How to load web fonts to avoid performance issues and speed up page loading"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.google.com/webfonts/",
-                          "Google Web Fonts"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.hongkiat.com/blog/beautiful-free-fonts-for-titles-and-headlines/",
-                          "Fonts for Titles and Headlines"
-                        )}
-                        {this.renderExternalLink(
-                          "https://www.sarasoueidan.com/blog/hex-rgb-to-hsl/",
-                          "On Switching from HEX & RGB to HSL"
-                        )}
-                        {this.renderExternalLink(
-                          "https://speckyboy.com/css-background-effects/",
-                          "10 Snippets for Creating Unique Background Effects with CSS"
-                        )}
-                      </ul>
-                    </AccordionItemPanel>
-                  </AccordionItem>
-                  <AccordionItem>
-                    <AccordionItemHeading>
-                      <AccordionItemButton>
-                        <span /> Web Design Inspitation Resources
-                      </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                      <ul>
-                        {this.renderExternalLink(
-                          "https://smashingmagazine.com/",
-                          "Smashing Magazine"
-                        )}
-                        {this.renderExternalLink(
-                          "https://www.mdgadvertising.com/resources-and-tools-for-designers/",
-                          "Resources and Tools for Designers"
-                        )}
-                        {this.renderExternalLink(
-                          "https://99designs.com/blog/trends/web-design-trends-2019/",
-                          "Current Web Design Trends"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.flickr.com/photos/splat/collections/72157600060481506/",
-                          "Patrick Haney&quot;s collection"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.flickr.com/groups/webdesign-inspiration/",
-                          "Web Design Inspiration flickr Pool"
-                        )}
-                        {this.renderExternalLink(
-                          "http://vandelaydesign.com/blog/design/flickr-groups-for-designers/",
-                          "Vandelay Design's list of 99 Flickr groups for design inspiration."
-                        )}
-                        {this.renderExternalLink(
-                          "https://zurb.com/",
-                          "UI Examples"
-                        )}
-                        {this.renderExternalLink(
-                          "http://factoryjoe.com/",
-                          "Chris Messina's web site"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.flickr.com/photos/factoryjoe/collections/72157600001823120/",
-                          "Collection of UI Design on Flickr by Chris Messina"
-                        )}
-                        {this.renderExternalLink(
-                          "http://designreviver.com/",
-                          "Design Reviver"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.lukew.com/resources/web_form_design.asp",
-                          " Web Form Design"
-                        )}
-                        {this.renderExternalLink(
-                          "http://colorschemedesigner.com/",
-                          "Color Schema Designer"
-                        )}
-                        {this.renderExternalLink(
-                          "https://color.adobe.com/create",
-                          "Adobe Kuler Color Schema"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.webdesignerwall.com/tutorials/css-decorative-gallery/",
-                          "Border Styles for Images"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.sitepoint.com/article/eight-definitive-font-stacks/",
-                          "Typography on the Web"
-                        )}
-                        {this.renderExternalLink(
-                          "http://mezzoblue.com/tests/revised-image-replacement/",
-                          "Revised Image Replacement"
-                        )}
-                        {this.renderExternalLink(
-                          "https://kilianvalkhof.com/2017/design/sloped-edges-with-consistent-angle-in-css/",
-                          "Sloped Edges With Consistent Angle in CSS"
-                        )}
-                        {this.renderExternalLink(
-                          "https://css-tricks.com/creating-non-rectangular-headers/",
-                          "Creating Non-Rectangular Headers"
-                        )}
-                        {this.renderExternalLink(
-                          "http://simplebits.com/",
-                          "Examples of decorative fonts"
-                        )}
-                        {this.renderExternalLink(
-                          "http://ilovetypography.com",
-                          "I Love Typography"
-                        )}
-                        {this.renderExternalLink(
-                          "http://designr.it",
-                          "Example of decorative text"
-                        )}
-                        {this.renderExternalLink(
-                          "http://www.alistapart.com/articles/writingainterfacestyleguide/",
-                          "Writing an Interface Style Guide"
-                        )}
-                        {this.renderExternalLink(
-                          "http://3oneseven.com/23/milo-web-design-expertise/",
-                          "Milo Web Design"
-                        )}
-                      </ul>
-                    </AccordionItemPanel>
-                  </AccordionItem>
-                  <AccordionItem>
-                    <AccordionItemHeading>
-                      <AccordionItemButton>
-                        <span /> Photography
-                      </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                      <ul>
-                        {this.renderExternalLink(
-                          "http://digital-photography-school.com/",
-                          "Digital Photography School"
-                        )}
-                        {this.renderExternalLink(
-                          "http://24ways.org/",
-                          "24ways.org"
-                        )}
-                      </ul>
-                    </AccordionItemPanel>
-                  </AccordionItem>
+                  {linksData.allLinks.map(item =>
+                    this.renderSection(item.section, item.links)
+                  )}
                 </Accordion>
               </div>
             </Lists>
